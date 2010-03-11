d2e91850369624ddf344825cd96bb61cf7096cb2
Graham.Dumpleton <Graham.Dumpleton@localhost>
2010-03-11 03:19:54 +0000

    Disable wsgi-handler code for now.

--- mod_wsgi/mod_wsgi.c
+++ mod_wsgi/mod_wsgi.c
@@ -8945,10 +8945,12 @@ static int wsgi_hook_handler(request_rec *r)
         }
     }
 #if AP_SERVER_MAJORVERSION_NUMBER >= 2
+#if 0
     else if (strstr(r->handler, "wsgi-handler=") == r->handler) {
         config->handler_script = apr_pstrcat(r->pool, r->handler+13, NULL);
         config->callable_object = "handle_request";
     }
+#endif
     else if (config->handler_scripts) {
         WSGIScriptFile *entry;
 
